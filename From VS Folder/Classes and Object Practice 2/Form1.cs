68b85552bb72f18e0ba275e1df359b6456d5f974
Shane Overby <dsoverby1986@gmail.com>
2014-10-09 11:41:15 -0400

    Initial Commit

---
+++ From VS Folder/Classes and Object Practice 2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classes_and_Object_Practice_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //HappyBirthday birthdayMessage;
+            //birthdayMessage = new HappyBirthday();
+
+            //MessageBox.Show(birthdayMessage.getMessage("Shane"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HappyBirthday birthdayMessage = new HappyBirthday();
+            string returnedMessage;
+
+            birthdayMessage.PresentCount = 10;
+            birthdayMessage.MyProperty = ("Shane");
+            birthdayMessage.PartyStatus = true; //Why is this not working properly?...Isn't this new value supposed to be passed over to the "PartyStatus" property variable in the HappyBirthday class?....Same goes for the code beneath.....WTF?
+            birthdayMessage.PartyAttendees = 27; //Why is this not working properly?...It seems to be set up the same as the value assignment for birthdayMessage.PresentCount
+            returnedMessage = birthdayMessage.MyProperty;
+
+            MessageBox.Show(returnedMessage);
+        }
+    }
+}
---
+++ From VS Folder/Classes and Object Practice 2/HappyBirthday.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Object_Practice_2
+{
+    class HappyBirthday                         //WHEN THE TERM "PROPERLY" COMES UP IN THE COMMENTS BELOW, I UNDERSTAND THAT
+    {                                           //ALL OF THE CODE HERE ACTUALLY IS WORKING PPROPERLY AND THAT  I AM JUST AN IDIOT AND
+        private int numberOfPresents;           //CAN'T FIGURE OUT ON MY OWN WHAT I  HAVE DONE WRONG WHILE I WAS WRITING THIS CODE.
+        private string birthdayMessage;
+        private bool partyYayNay;
+        private int partyComers;
+
+        public HappyBirthday()
+        {
+            numberOfPresents = 0;
+            partyYayNay = false; // Why is this not working properly?...I thought that this is just a default value that can be changed when the property that this variable belongs to is called up in the button code and assigned a new value....it doesn't change though.....it just remains whatever value is assigned in the class constructor....WTF?
+            partyComers = 0; //Why is this not working properly?...it seems to be set up the same as the numberOfPresents variable.
+        }
+
+        private string getMessage(string givenName)//
+        {
+            string theMessage;
+
+            theMessage = "Happy Birthday " + givenName + "\n";
+            theMessage += "Number of presents = " + numberOfPresents.ToString() + "\n";
+            if(partyYayNay == true)
+            {
+                theMessage += "Hope you enjoy the party!" + "\n";
+            }
+            else
+            {
+                theMessage += "No Party - Sorry!" + "\n";
+            }
+            theMessage += "Number of people attending your party = " + partyComers.ToString();
+            return theMessage;
+        }
+
+        public string MyProperty
+        {
+            get { return birthdayMessage; }
+            set { birthdayMessage = getMessage(value); } //Is this "(value)" where the birthday boy/girl name from the button code is being passed over to by saying 'birthdayMessage.MyProperty = "Shane";' and then constructed using the "givenName" variable from the getMessage method?
+        }
+
+        public int PresentCount 
+        { 
+            set { numberOfPresents = value; } 
+        }
+        
+        public bool PartyStatus
+        {
+            set { partyYayNay = value; }
+        }
+
+        public int PartyAttendees //Why is this not working properly?...it seems to be set up the same as the PresentCount property.
+        {
+            set { partyComers = value; }
+        }
+    }
+}
